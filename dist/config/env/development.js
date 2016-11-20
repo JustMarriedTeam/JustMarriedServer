968592df10fcbc73d0107e83a427c56aefe6d86a
kboom <gurgul.grzegorz@gmail.com>
2016-11-20 20:23:09 +0100

    Initial commit

---
+++ dist/config/env/development.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  env: 'development',
+  MONGOOSE_DEBUG: true,
+  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+  db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
+  port: 4040
+};
+module.exports = exports['default'];
+//# sourceMappingURL=development.js.map
---
+++ dist/config/env/production.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  env: 'production',
+  jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
+  db: 'mongodb://localhost/express-mongoose-es6-rest-api-production',
+  port: 4040
+};
+module.exports = exports['default'];
+//# sourceMappingURL=production.js.map
