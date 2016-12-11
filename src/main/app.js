615206de02702e0ce48d1d3c5571fc6f1e2cca4a
kboom <gurgul.grzegorz@gmail.com>
2016-12-11 16:30:43 +0100

    Refactored app.js

--- src/main/app.js
+++ src/main/app.js
@@ -1,87 +1,87 @@
-import express from 'express';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compress from 'compression';
-import methodOverride from 'method-override';
-import cors from 'cors';
-import httpStatus from 'http-status';
-import expressWinston from 'express-winston';
-import expressValidation from 'express-validation';
-import helmet from 'helmet';
-import winstonInstance from './config/winston';
-import routes from './routes/index.route';
-import APIError from './helpers/APIError';
+import express from "express";
+import logger from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import methodOverride from "method-override";
+import cors from "cors";
+import httpStatus from "http-status";
+import expressWinston from "express-winston";
+import expressValidation from "express-validation";
+import helmet from "helmet";
+import winstonInstance from "./config/winston";
+import routes from "./routes/index.route";
+import APIError from "./helpers/APIError";
 
 const app = express();
 const env = process.env.env;
 
-if (env === 'development') {
-  app.use(logger('dev'));
+
+initializeLogging();
+configureOthers();
+configureErrorHandling();
+configureNotFoundBehaviour();
+mountPaths();
+
+function configureOthers() {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(methodOverride());
+    app.use(helmet());
+    app.use(cors());
 }
 
-// parse body params and attache them to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-app.use(compress());
-app.use(methodOverride());
-
-// secure apps by setting various HTTP headers
-app.use(helmet());
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-// enable detailed API logging in dev env
-if (env === 'development') {
-  expressWinston.requestWhitelist.push('body');
-  expressWinston.responseWhitelist.push('body');
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }));
+function configureNotFoundBehaviour() {
+    app.use((req, res, next) => {
+        const err = new APIError('API not found', httpStatus.NOT_FOUND);
+        return next(err);
+    });
 }
 
-// mount all routes on /api path
-app.use('/api', routes);
-
-// if error is not an instanceOf APIError, convert it.
-app.use((err, req, res, next) => {
-  if (err instanceof expressValidation.ValidationError) {
-    // validation error contains errors which is an array of error each containing message[]
-    const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
-    const error = new APIError(unifiedErrorMessage, err.status, true);
-    return next(error);
-  } else if (!(err instanceof APIError)) {
-    const apiError = new APIError(err.message, err.status, err.isPublic);
-    return next(apiError);
-  }
-  return next(err);
-});
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new APIError('API not found', httpStatus.NOT_FOUND);
-  return next(err);
-});
-
-// log error in winston transports except when executing test suite
-if (env !== 'test') {
-  app.use(expressWinston.errorLogger({
-    winstonInstance
-  }));
+function configureErrorHandling() {
+    app.use((err, req, res, next) => {
+        if (err instanceof expressValidation.ValidationError) {
+            const unifiedErrorMessage = err.errors.map(error => error.messages.join('. ')).join(' and ');
+            const error = new APIError(unifiedErrorMessage, err.status, true);
+            return next(error);
+        } else if (!(err instanceof APIError)) {
+            const apiError = new APIError(err.message, err.status, err.isPublic);
+            return next(apiError);
+        }
+        return next(err);
+    });
+    app.use((err, req, res, next) =>
+        res.status(err.status).json({
+            message: err.isPublic ? err.message : httpStatus[err.status],
+            stack: process.env.env === 'development' ? err.stack : {}
+        })
+    );
 }
 
-// error handler, send stacktrace only during development
-app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
-  res.status(err.status).json({
-    message: err.isPublic ? err.message : httpStatus[err.status],
-    stack: process.env.env === 'development' ? err.stack : {}
-  })
-);
+function initializeLogging() {
+    if (env === 'development') {
+        app.use(logger('dev'));
+        expressWinston.requestWhitelist.push('body');
+        expressWinston.responseWhitelist.push('body');
+        app.use(expressWinston.logger({
+            winstonInstance,
+            meta: true,
+            msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+            colorStatus: true
+        }));
+    }
+
+    if (env !== 'test') {
+        app.use(expressWinston.errorLogger({
+            winstonInstance
+        }));
+    }
+}
+
+function mountPaths() {
+    app.use('/api', routes);
+}
 
 export default app;
