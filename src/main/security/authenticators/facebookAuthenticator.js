f47cdf43e5048ac59ecf719cf9ced6cae3a0db04
kboom <gurgul.grzegorz@gmail.com>
2016-12-12 19:44:19 +0100

    Added fb auth

--- src/main/security/authenticators/facebookAuthenticator.js
+++ src/main/security/authenticators/facebookAuthenticator.js
@@ -2,8 +2,8 @@ import passport from "passport";
 import FacebookStrategy from "passport-facebook";
 
 passport.use(new FacebookStrategy({
-    clientID: 'abc',
-    clientSecret: 'secret',
+    clientID: '1806015219657884',
+    clientSecret: '1e9d67e72737902cce62420e268d5a82',
     callbackURL: '/api/facebook/',
     profileFields: ["emails", "displayName"]
 }));
