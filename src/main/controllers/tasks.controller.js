58e61b5c976a87fc42bf1a6e202f9ac333a552a4
kboom <gurgul.grzegorz@gmail.com>
2016-12-14 15:49:05 +0100

    Adding tasks and retriving them

--- src/main/controllers/tasks.controller.js
+++ src/main/controllers/tasks.controller.js
@@ -1,15 +1,17 @@
 import {Task} from "../models/task.model";
+import { createTask, listTasks } from '../services/task.service'
 
 function getTasks(req, res, done) {
-    res.status(200).json([{
-        name: 'x'
-    }]);
+    listTasks(req.query).then((tasks) => res.status(200).json(tasks))
+        .catch((err) => res.status(400).json(err))
+        .finally(done);
 }
 
 function postTask(req, res, done) {
-    res.status(200).json([{
-        name: 'x'
-    }]);
+    createTask(req.body)
+        .then((task) => res.status(200).json(task))
+        .catch((err) => res.status(400).json(err))
+        .finally(done);
 }
 
 export {getTasks, postTask};
