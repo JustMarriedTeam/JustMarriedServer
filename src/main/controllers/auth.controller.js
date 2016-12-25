1921ce025f331a9c07a66be2c64879534cecb018
kboom <gurgul.grzegorz@gmail.com>
2016-12-25 19:38:09 +0100

    Sending token as a query param in the redirect.

--- src/main/controllers/auth.controller.js
+++ src/main/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import jwtManager from "../security/jwt/jwtManager";
 function getReleasedToken(req, res) {
     let token = jwtManager.releaseToken(req.user);
     res.cookie('authToken', token.token);//.status(200).json(token);
-    res.redirect('http://localhost:3000/redirect.html');
+    res.redirect(`http://localhost:3000/redirect.html?token=${token.token}`);
 }
 
 export {getReleasedToken};
