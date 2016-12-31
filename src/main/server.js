3cc879c9c673fbbddc240286db620bf9b83ade89
kboom <gurgul.grzegorz@gmail.com>
2016-12-31 13:34:49 +0100

    enabled logging on staging env

--- src/main/server.js
+++ src/main/server.js
@@ -11,4 +11,5 @@ logger.info(
     `
 );
 
-export const SERVER_URI = `${process.env.protocol}://${process.env.host}` + !process.env.domain ? `:${process.env.port}` : '';
+export const SERVER_URI = `${process.env.protocol}://${process.env.host}`
+    + (!process.env.domain ? `:${process.env.port}` : '');
