4f076ae0d75d77939527b051927c38438ddc7740
kboom <gurgul.grzegorz@gmail.com>
2016-12-31 13:57:39 +0100

    parameterized token

--- src/main/controllers/auth.controller.js
+++ src/main/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import jwtManager from "../security/jwt/jwtManager";
+import { format } from 'util';
+import properties from '../properties'
 
 function getReleasedToken(req, res) {
     let token = jwtManager.releaseToken(req.user);
     res.cookie('authToken', token.token);//.status(200).json(token);
-    res.redirect(`http://localhost:3000/redirect.html?token=${token.token}`);
+    res.redirect(format(properties.authRedirectUrl, token.token));
 }
 
 export {getReleasedToken};
--- src/main/properties.js
+++ src/main/properties.js
@@ -2,7 +2,8 @@ import envFile from 'node-env-file';
 import extend from 'lodash/extend';
 import pick from 'lodash/pick';
 
-const passedEnvVariables = pick(process.env, 'logLevel', 'protocol', 'host', 'port', 'domain', 'envPropsFile', 'dbUrl', 'jwtSecret');
+const passedEnvVariables = pick(process.env,
+    'logLevel', 'protocol', 'host', 'port', 'domain', 'envPropsFile', 'dbUrl', 'jwtSecret');
 envFile(`${__dirname}/env.properties`);
 const envPropsFile = passedEnvVariables['envPropsFile'];
 if(!!envPropsFile) {
