82050945d1bd209b47852d7a26a552d55ec3168a
kboom <gurgul.grzegorz@gmail.com>
2017-01-01 18:16:14 +0100

    limit query test

--- src/test/integration/tasks.integration.js
+++ src/test/integration/tasks.integration.js
@@ -3,8 +3,9 @@
 import Promise from "bluebird";
 import request from "supertest";
 import httpStatus from "http-status";
-import chai, { expect } from "chai";
+import chai, {expect} from "chai";
 import app from "../../main/index";
+import forEach from "lodash/forEach";
 
 import {
   blueAccount,
@@ -20,7 +21,7 @@ import {
   tearDownTasks
 } from "../data/tasks.data";
 
-import { getTokenFor } from "../utils/auth.utils";
+import {getTokenFor} from "../utils/auth.utils";
 
 chai.config.includeStack = true;
 
@@ -29,11 +30,11 @@ describe("Tasks", () => {
   let token;
 
   before(() => Promise.join(
-      setUpAccounts(blueAccount),
-      setUpTasks(redTask, greenTask, blueTask),
-        (account) => {
-          token = getTokenFor(account);
-        }
+    setUpAccounts(blueAccount),
+    setUpTasks(redTask, greenTask, blueTask),
+    (account) => {
+      token = getTokenFor(account);
+    }
   ));
 
   after(() => Promise.join(
@@ -50,17 +51,47 @@ describe("Tasks", () => {
         .expect(httpStatus.OK)
         .then((res) => {
           expect(res.body).to.have.lengthOf(3);
+          forEach([
+            {
+              "name": "red task",
+              "status": "blocked"
+            },
+            {
+              "name": "green task",
+              "status": "pending"
+            },
+            {
+              "name": "blue task",
+              "status": "done"
+            }
+          ], (task, index) => expect(res.body[index]).to.be.deep.equal(task));
         })
     );
 
     it("can limit tasks got", () =>
       request(app)
         .get("/api/tasks")
-        .query({ limit: 1 })
+        .query({limit: 1})
         .set("token", token)
         .expect(httpStatus.OK)
         .then((res) => {
           expect(res.body).to.have.lengthOf(1);
+        })
+    );
+
+    it("can offset tasks got", () =>
+      request(app)
+        .get("/api/tasks")
+        .query({offset: 1, limit: 1})
+        .set("token", token)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body).to.be.deep.equal([
+            {
+              "name": "red task",
+              "status": "blocked"
+            }
+          ]);
         })
     );
 
