61b4aa54031bf14a80286dccbfd0b0ac87734f75
kboom <gurgul.grzegorz@gmail.com>
2017-04-17 15:47:52 +0200

    Dump & restore staging db

--- database/scripts/remote/restore_remote.sh
+++ database/scripts/remote/restore_remote.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-RESTORE_SCRIPT="mongorestore --drop"
+RESTORE_SCRIPT="mongorestore -h ds141428.mlab.com:41428 -d heroku_wsm0k87c -u ggurgul -p ggurgul /dump/staging"
 docker exec just-married-db bash -c "eval $RESTORE_SCRIPT"
 
