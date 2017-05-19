4df44c9f2ad31c230c63d7cff8cffc906d369c3b
kboom <gurgul.grzegorz@gmail.com>
2017-05-19 19:43:31 +0200

    Adding missing files.

---
+++ containers/build/build.entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function exit_with_error {
+	echo "$1" 1>&2
+	exit 1
+}
+
+echo "Building artifact <${ARTIFACT_NAME}>"
+
+cd app
+npm install || exit_with_error "Could not install dependencies!"
+npm run dist || exit_with_error "Could not build project!"
+tar -cvf ${OUTPUT_DIR}/${ARTIFACT_NAME}.tar node_modules -C dist . || exit_with_error "Could not save artifact!"
+
+echo "Successfully built and stored artifact <${ARTIFACT_NAME}>"
+echo "${OUTPUT_DIR}/${ARTIFACT_NAME}.tar"
